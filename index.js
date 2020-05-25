bc0e98ba4bc5dbf076fd7cd11679a903cc4454fe
nibles1998 <longvynguyenhoang@gmail.com>
2020-05-25 23:18:22 +0700

    Modify Index

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const { PORT } = require('./config');
+const bodyParser = require('body-parser');
 
 const app = express();
 const admin = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(express.static('src/travelco'));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.sendFile('src/travelco/index.html', { root: __dirname });
 });
 
 admin.use(express.static('src/RuangAdmin-master'));
 
 admin.get('/', (req, res) => {
-    res.render('index');
+    res.sendFile('src/RuangAdmin-master/index.html', { root: __dirname });
 });
 
 app.use('/admin', admin);
